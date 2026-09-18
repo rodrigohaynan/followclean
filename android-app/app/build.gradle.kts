49826ddefeef9d050908a241f3803c0339cc63ba
rodrigohaynan <rodrigo_haynan@hotmail.com>
2026-09-18 07:59:46 -0400

    chore(android): bump APK to 0.3.9

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "br.com.followclean.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "0.3.8"
+        versionCode = 15
+        versionName = "0.3.9"
     }
 
     buildTypes {
