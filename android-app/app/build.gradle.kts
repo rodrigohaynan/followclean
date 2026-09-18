0495a8877829c0cfa74c5c33160aa72ee8b419e4
rodrigohaynan <rodrigo_haynan@hotmail.com>
2026-09-17 21:29:17 -0400

    feat(android): add android-app/app/build.gradle.kts

---
+++ android-app/app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "br.com.followclean.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "br.com.followclean.app"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.webkit:webkit:1.12.1")
+}
